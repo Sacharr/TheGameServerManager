d3381e25e5b238ab5e42d0c98a3f032fd9c24791
SaltyCuban <137945152+SaltyCuban@users.noreply.github.com>
2023-07-09 09:11:44 -0400

    update steamcmd button (#11)
    * i have added button, named it, then on click event i have added code for it to open steamcmd.exe
    
    * i added the +quit argument so steamcmd closes after it updates.
    
    * deleted the dead code

--- ValheimServer/ValheimServer/MainWindow.xaml.cs
+++ ValheimServer/ValheimServer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,12 @@ namespace ValheimServer
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SteamcmdButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Process.Start("steamcmd.exe", "+quit");
         }
     }
 }

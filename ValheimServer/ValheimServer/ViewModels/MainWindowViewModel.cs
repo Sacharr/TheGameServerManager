1dab4ef7290d36df76b90b231a93eb34d31d83d8
Nick <tatassupra98@hotmail.com>
2024-05-23 21:01:39 -0400

    button starts process now and opens the correct exe

--- ValheimServer/ValheimServer/ViewModels/MainWindowViewModel.cs
+++ ValheimServer/ValheimServer/ViewModels/MainWindowViewModel.cs
@@ -160,7 +160,7 @@ namespace ValheimServer.ViewModels
         [RelayCommand]
         private void ServerStartButtonClick()
         {
-            Process.Start("C:\\ValheimServer\valheim_server.exe", "-nographics -batchmode -name \"USER_ENTERED_NAME\" -port USER_ENTERED_PORT -world \"USER_ENTERED_WORLD\" -password \"USER_ENTERED_PW\" -public 1\"");
+            Process.Start("C:\\valheimserver\\valheim_server.exe", "-nographics -batchmode -name \"USER_ENTERED_NAME\" -port USER_ENTERED_PORT -world \"USER_ENTERED_WORLD\" -password \"USER_ENTERED_PW\" -public 1\"");
         }
 
 

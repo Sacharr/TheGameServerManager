feac50ec486cb0fbff7445b0b9d7b0b3f2789e9d
Sach <sachinn89@gmail.com>
2024-09-06 19:18:12 -0400

    Modify stylecop rules and analyzers. Format code to follow new rules

--- ValheimServer/ValheimServer/App.xaml.cs
+++ ValheimServer/ValheimServer/App.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows;
 namespace ValheimServer
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Interaction logic for App.xaml.
     /// </summary>
     public partial class App : Application
     {
--- ValheimServer/ValheimServer/ViewModels/MainWindowViewModel.cs
+++ ValheimServer/ValheimServer/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-//using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -14,27 +13,38 @@ using System.Windows.Media.Animation;
 
 namespace ValheimServer.ViewModels
 {
+    /// <summary>
+    /// Represents the main window view model.
+    /// </summary>
     public partial class MainWindowViewModel : ObservableObject
     {
+        private bool _uselessCheckBox = false;
+        private string _installFolder;
+        private string _serverTextBox;
+        private string _folderName;
+        private string _portTextBox;
+        private string _worldTextBox;
+        private string _passwordTextBox;
+        private string _hiddenTextBox;
+        private bool _serverVisibilityCheckBox = true;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+        /// </summary>
         public MainWindowViewModel()
         {
-
             _installFolder = AppSettings.Default.InstallFolder;
             _serverTextBox = AppSettings.Default.ServerName;
             _passwordTextBox = AppSettings.Default.Password;
             _portTextBox = AppSettings.Default.PortNumber;
             _worldTextBox = AppSettings.Default.WorldName;
-
+            _folderName = string.Empty;
+            _hiddenTextBox = string.Empty;
         }
 
-        [RelayCommand]
-        private void SteamButtonClick()
-        {
-            Process.Start("steamcmd.exe", "+quit");
-        }
-        private bool _uselessCheckBox = false;
-
+        /// <summary>
+        /// Gets or Sets a value indicating whether the Checkbox is checked.
+        /// </summary>
         public bool UselessCheckBox
         {
             get
@@ -46,6 +56,155 @@ namespace ValheimServer.ViewModels
             {
                 SetProperty(ref _uselessCheckBox, value);
             }
+        }
+
+        /// <summary>
+        /// Gets or sets the install folder.
+        /// </summary>
+        public string InstallFolder
+        {
+            get
+            {
+                return _installFolder;
+            }
+
+            set
+            {
+                SetProperty(ref _installFolder, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the server name.
+        /// </summary>
+        public string ServerTextBox
+        {
+            get
+            {
+                return _serverTextBox;
+            }
+
+            set
+            {
+                SetProperty(ref _serverTextBox, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the folder name.
+        /// </summary>
+        public string FolderName
+        {
+            get
+            {
+                return _folderName;
+            }
+
+            set
+            {
+                SetProperty(ref _folderName, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the port number.
+        /// </summary>
+        public string PortTextBox
+        {
+            get
+            {
+                return _portTextBox;
+            }
+
+            set
+            {
+                SetProperty(ref _portTextBox, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the world name.
+        /// </summary>
+        public string WorldTextBox
+        {
+            get
+            {
+                return _worldTextBox;
+            }
+
+            set
+            {
+                SetProperty(ref _worldTextBox, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or Sets the password.
+        /// </summary>
+        public string PaswordTextBox
+        {
+            get
+            {
+                return _passwordTextBox;
+            }
+
+            set
+            {
+                SetProperty(ref _passwordTextBox, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or Sets the hidden text box.
+        /// </summary>
+        public string HiddenTextBox
+        {
+            get
+            {
+                return _hiddenTextBox;
+            }
+
+            set
+            {
+                SetProperty(ref _hiddenTextBox, value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or Sets a value indicating whether the checkbox is visible.
+        /// </summary>
+        public bool ServerVisibilityCheckBox
+        {
+            get
+            {
+                return _serverVisibilityCheckBox;
+            }
+
+            set
+            {
+                SetProperty(ref _serverVisibilityCheckBox, value);
+            }
+        }
+
+        private int ServerVisibiltyCheck
+        {
+            get
+            {
+                if (_serverVisibilityCheckBox == true)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        [RelayCommand]
+        private void SteamButtonClick()
+        {
+            Process.Start("steamcmd.exe", "+quit");
         }
 
         [RelayCommand]
@@ -60,41 +219,9 @@ namespace ValheimServer.ViewModels
                 InstallFolder = folderBrowserDialog.SelectedPath;
                 AppSettings.Default.InstallFolder = InstallFolder;
                 AppSettings.Default.Save();
-
             }
 
             Process.Start("steamcmd.exe", $"+login anonymous +force_install_dir {_installFolder} +app_update 896660 validate +quit");
-        }
-
-        private string _installFolder;
-
-        public string InstallFolder
-        {
-            get
-            {
-                return _installFolder;
-            }
-
-            set
-            {
-                SetProperty(ref _installFolder, value);
-            }
-        }
-
-
-        private string _serverTextBox;
-
-        public string ServerTextBox
-        {
-            get
-            {
-                return _serverTextBox;
-            }
-
-            set
-            {
-                SetProperty(ref _serverTextBox, value);
-            }
         }
 
         [RelayCommand]
@@ -117,140 +244,23 @@ namespace ValheimServer.ViewModels
             }
         }
 
-
-        private string _folderName;
-
-        public string FolderName
-        {
-            get
-            {
-                return _folderName;
-            }
-
-            set
-            {
-                SetProperty(ref _folderName, value);
-            }
-        }
-
-        private string _portTextBox;
-
-        public string PortTextBox
-        {
-            get
-            {
-                return _portTextBox;
-            }
-
-            set
-            {
-                SetProperty(ref _portTextBox, value);
-            }
-        }
-
-        private string _worldTextBox;
-
-        public string WorldTextBox
-        {
-            get
-            {
-                return _worldTextBox;
-            }
-
-            set
-            {
-                SetProperty(ref _worldTextBox, value);
-            }
-
-        }
-
-        private string _passwordTextBox;
-
-        public string PaswordTextBox
-        {
-            get
-            {
-                return _passwordTextBox;
-            }
-
-            set
-            {
-                SetProperty(ref _passwordTextBox, value);
-            }
-
-        }
-
-        private string _hiddenTextBox;
-
-        public string HiddenTextBox
-        {
-            get
-            {
-                return _hiddenTextBox;
-            }
-
-            set
-            {
-                SetProperty(ref _hiddenTextBox, value);
-            }
-
-        }
-
-        private bool _serverVisibilityCheckBox = true;
-
-        public bool ServerVisibilityCheckBox
-        {
-            get
-            {
-                return _serverVisibilityCheckBox;
-            }
-
-            set
-            {
-                SetProperty(ref _serverVisibilityCheckBox, value);
-            }
-
-        }
-
-        private int _serverVisibiltyCheck
-        {
-            get
-            {
-                if (_serverVisibilityCheckBox == true)
-                {
-                    return 1;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-        }
-
-
-
         [RelayCommand]
         private void ServerStartButtonClick()
         {
-        
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
                 FileName = "cmd.exe",
-                Arguments = $@"/K set SteamAppId=892970 && {_installFolder}\\valheim_server.exe -nographics -batchmode -name ""{ _serverTextBox }"" -port {_portTextBox} -world ""{_worldTextBox}"" -password ""{_passwordTextBox}"" -public {_serverVisibiltyCheck}",
-                UseShellExecute = false
+                Arguments = $@"/K set SteamAppId=892970 && {_installFolder}\\valheim_server.exe -nographics -batchmode -name ""{_serverTextBox}"" -port {_portTextBox} -world ""{_worldTextBox}"" -password ""{_passwordTextBox}"" -public {ServerVisibiltyCheck}",
+                UseShellExecute = false,
             };
-           
+
             Process.Start(startInfo);
 
             AppSettings.Default.ServerName = ServerTextBox;
             AppSettings.Default.Password = PaswordTextBox;
-            AppSettings.Default.PortNumber = PortTextBox;  
+            AppSettings.Default.PortNumber = PortTextBox;
             AppSettings.Default.WorldName = WorldTextBox;
             AppSettings.Default.Save();
-
         }
-
-
     }
-
 }

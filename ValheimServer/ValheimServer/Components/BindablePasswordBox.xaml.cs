99aa16b1b1987d5c339f456b8a1358e74292707f
Nick <tatassupra98@hotmail.com>
2024-09-05 21:14:19 -0400

    password box has been created and its passing value to the variable.

--- ValheimServer/ValheimServer/Components/BindablePasswordBox.xaml.cs
+++ ValheimServer/ValheimServer/Components/BindablePasswordBox.xaml.cs
@@ -20,9 +20,29 @@ namespace ValheimServer.Components
     /// </summary>
     public partial class BindablePasswordBox : UserControl
     {
+
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Password.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox), new PropertyMetadata(string.Empty));
+
+
+
+
         public BindablePasswordBox()
         {
             InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = passwordBox.Password;
         }
     }
 }
